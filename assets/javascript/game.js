c6824a1b03b6215500d7a1a5ad780c74b604904b
SNelson <snelson1337@gmail.com>
2019-08-01 10:04:03 -0500

    Started working on the html javascript interactions for the display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 const starWords = ["Jabba The Hutt", "Mos Eisely Cantina", "Harrison Ford", "Carrie Fisher",
  "Skywalker", "Sith Lord", "Jedi Master", "Shii Cho", "Makashi", "Soresu", "Ataru", "Niman",
   "Juyo", "Yoda", "Millennium Falcon", "X Wing Starfighter", "Galactic Republic", 
-  "Corellian Run", "Coruscant", "Exar Kun", "Trade Federation"];
+  "Corellian Run", "Coruscant", "Exar Kun", "Trade Federation", "Admiral Ackbar"];
 
 let score = 0;
 let loss = false;
@@ -11,6 +11,10 @@ let correctGuess = false;
 let rightLetters = 0;
 let guesses = 5;
 let word = starWords[Math.floor(Math.random() * starWords.length)];
+let userAlphabet = [];
+const userScore = document.getElementById("userScore");
+const userGuesses = document.getElementById("guesses");
+const userAlphabetDisplay = document.getElementById("userGuesses")
 word = word.toLowerCase();
 
 // Function to log out what the letters of the word are
@@ -20,6 +24,8 @@ function report(str){
 
 //Function that readies for a new game if the user wins or loses
 function ready(){
+    userGuesses.textContent = "5";
+    userAlphabet = [];
     word = starWords[Math.floor(Math.random() * starWords.length)];
     guesses = 5;
     loss = false;
@@ -44,6 +50,7 @@ document.onkeyup = function(event){
     }
     else if(loss !== true || rightLetters !== word.length){
         const userGuess = event.key;
+        userAlphabet.push(userGuess);
         console.log("You guessed: " + userGuess);
         for(let i = 0; i < word.length; i++){
             if(userGuess === word[i]){
@@ -56,6 +63,7 @@ document.onkeyup = function(event){
     
         if(correctGuess !== true){
             guesses--;
+            userGuesses.textContent = toString(guesses);
             console.log("Oh no you only have: " + guesses + " guesses left!");
         }
     
